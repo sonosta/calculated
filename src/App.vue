2ef38a004ce996d485d2eb0c7194a6935966427b
Nikita Arinchekhin <narincehin@gmail.com>
2025-10-02 21:22:01 +0300

    check user by login and passaword

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,43 @@
 <template>
-  Логин: <input></input>
-  Пароль: <input ></input>
+  Логин: <input v-model="login"></input>
+  Пароль: <input v-model="password"></input>
+  <button @click="getUser">Проверка</button>
 </template>
 
 <script>
 export default {
   name: 'App', 
+  data(){
+    return{
+      login:'',
+      password:'',
+    }
+  },
+  methods:{
+    async getUser(){
+      try {
+        const response = await fetch('http://localhost:3000/api/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            login: this.login,
+            password: this.password
+          })
+        });
+        const data = await response.json();
+        if (response.ok) {
+          console.log('Успешный вход:', data);
+          // Например, сохранить токен и данные пользователя
+          // localStorage.setItem('token', data.token);
+        } else {
+          this.error = data.message || 'Ошибка входа';
+        }
+      } catch (err) {
+        this.error = 'Ошибка сети: ' + err.message;
+      }
+    }
+  }
 }
 </script>
